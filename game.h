b76065c21681f6e9caf81550682f6059b63a097d
İbrahim Yiğit Öztemür <116972349+d1sglair@users.noreply.github.com>
2026-05-29 14:33:57 +0300

    Add game.h header file for paddle game

---
+++ game.h
@@ -0,0 +1,34 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <SDL2/SDL.h>
+
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+
+#define PADDLE_WIDTH 15
+#define PADDLE_HEIGHT 100
+#define PADDLE_SPEED 8
+
+typedef struct
+{
+    float x;
+    float y;
+    int width;
+    int height;
+} Paddle;
+
+void handleInput(SDL_Event *event,
+                 Paddle *leftPaddle,
+                 Paddle *rightPaddle,
+                 int *running);
+
+void updatePaddles(const Uint8 *keyboardState,
+                   Paddle *leftPaddle,
+                   Paddle *rightPaddle);
+
+void render(SDL_Renderer *renderer,
+            Paddle *leftPaddle,
+            Paddle *rightPaddle);
+
+#endif
